bc5dddefe40a56cbca6ddcf80a9f1d2a7ae040be
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-03-15 12:55:57 +0530

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,8 +4,8 @@ import Messages from '../components/Messages';
 function HomePage() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the Popeye React Communication App!</p>
+      <h1>popeye</h1>
+      <p>the developer</p>
       <Messages />
     </div>
   );
