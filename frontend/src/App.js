3a866fb5c50620abf95d5a1cafaa7bb957b36b84
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-04-27 17:09:51 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Chat from './pages/Chat';
+import Home from './pages/Home.js';
+import Chat from './pages/Chat.js';
 
 const App = () => (
   <Router>
