4e4adfca86affca07b57ea26e7810885ce3f4237
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-07-18 19:04:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,5 +14,39 @@ console.log(appLoadStarted);
 
 const greeting = document.createElement('div');
 greeting.setAttribute('id', 'greeting-card');
+greeting.textContent = 'welcome';
 
+// append greeting to app
 app.appendChild(greeting);
+
+const login = document.createElement('div');
+login.setAttribute('id', 'login');
+login.textContent = 'login';
+
+const loginlink = document.createElement('a');
+loginlink.setAttribute('href', '#login');
+loginlink.setAttribute('id', 'login-link');
+loginlink.textContent = login.textContent;
+
+const signup = document.createElement('div');
+signup.setAttribute('id', 'sign-up');
+signup.textContent = 'sign up';
+
+const signuplink = document.createElement('a');
+signuplink.setAttribute('href', '#sign-up');
+signuplink.setAttribute('id', 'signup-link');
+signuplink.textContent = signup.textContent;
+
+loginlink.addEventListener('click', () => {
+  app.remove(greeting)
+  app.remove(loginlink)
+  app.remove(signuplink)
+  app.appendChild(login)
+});
+
+signuplink.addEventListener('click', () => {
+  app.remove(greeting)
+  app.remove(loginlink)
+  app.remove(signuplink)
+  app.appendChild(signup)
+});
